4d14603cac02eb363c9a41bf4916fc993db5479b
ob198-CPU <obata1986@gmail.com>
2026-07-15 10:48:39 +0900

    管理画面10を作成し専用シートへ接続

---
+++ config.js
@@ -0,0 +1,5 @@
+﻿// Google Apps Script 縺ｮ繧ｦ繧ｧ繝悶い繝励ΜURL繧定ｨｭ螳壹☆繧九→縲∝・譛峨せ繝励Ξ繝・ラ繧ｷ繝ｼ繝井ｿ晏ｭ倥↓蛻・ｊ譖ｿ繧上ｊ縺ｾ縺吶・// 萓・ window.PRODUCTION_REPORT_API_URL = "https://script.google.com/macros/s/AKfycbzItQ9xpWsDeJ1e5y_bSa17O4kcDiSQkaQRxvxtzZWz-c9zsgAydqYTiDJhqQsKa0z1uA/exec";
+window.PRODUCTION_REPORT_API_URL = "https://script.google.com/macros/s/AKfycbxp0W8DIh4J_011diZBgcAJ-5hGAAsBE2Rd6BtD_SOtJBpLxdOaAQcOqleCzbYEGgOI/exec";
+
+
+
